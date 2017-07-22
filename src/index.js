8b28a1ae4473d91df9c890aaf6cb76abc85d26af
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-07-22 07:59:35 -0400

    Add boilerplate for skiDay>ADD_DAY reducer

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import C from './constants'
-import { goal } from './store/reducers'
+import { skiDay } from './store/reducers'
 
-const state = 10
+const state = null
 
 const action = {
-  type: C.SET_GOAL,
-  payload: 15
+  type: C.ADD_DAY,
+  payload: {
+    "resort": "Heavenly",
+    "date": "2017-07-23",
+    "powder": true,
+    "backcountry": false
+  }
 }
 
-const nextState = goal(state, action)
+const nextState = skiDay(state, action)
+
 console.log(`
-  initial goal: ${state}
+  initial state: ${state}
   action: ${JSON.stringify(action)}
-  new goal: ${nextState}
+  new state: ${JSON.stringify(nextState)}
 `)
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,9 +1,11 @@
 import C from '../constants'
 
-export const goal = (state=10, action) => {
-  if (action.type === C.SET_GOAL) {
-    return parseInt(action.payload)
-  } else {
-    return state
-  }
-}
+export const goal = (state=10, action) =>
+  action.type === C.SET_GOAL ?
+    parseInt(action.payload) :
+    state
+
+export const skiDay = (state=null, action) =>
+  action.type === C.ADD_DAY ?
+    action.payload :
+    state
